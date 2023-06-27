f649b204c5551aac603542d74df0ff0b5f4be967
Saimon Bhuiyan <120123701+saimon4u@users.noreply.github.com>
2023-06-27 19:08:19 +0600

    app

---
+++ index.js
@@ -0,0 +1,21 @@
+const app = require('express');
+const PORT = process.env.PORT || 3000;
+const http = require('http').Server(app);
+const io = require('socket.io')(http,{cors: {origin: "*"}});
+http.listen(PORT);
+const users = {};
+io.on('connection', socket=>{
+    socket.on('joined',id=>{
+        users[socket.id] = id;
+        socket.broadcast.emit('user-joined',id);
+    });
+
+    socket.on('send', message=>{
+        socket.broadcast.emit('receive',{message: message , id : users[socket.id]});
+    });
+
+    socket.on('disconnect', message=>{
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+})
