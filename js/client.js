11a547c1a5b2c71d5b347ce8b65ab6c63892461b
Saimon Bhuiyan <120123701+saimon4u@users.noreply.github.com>
2023-06-28 16:29:24 +0600

    final

--- js/client.js
+++ js/client.js
@@ -10,23 +10,32 @@ const append = (message,position)=>{
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
     if(position=='left')audio.play();
-}
+};
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
     const message = messageInput.value;
     append(`${message}`,'right');
     socket.emit(`send`,message);
     messageInput.value = '';
-})
+});
 
 const id = prompt("Enter your name to join");
-socket.emit('joined', id);
+const join = document.getElementById('join');
+const joined = document.getElementById('joined');
+const show = document.getElementById('show');
+join.addEventListener('click', (e)=>{
+    e.preventDefault();
+    socket.emit('joined',id);
+    joined.classList.remove('v-class');
+    join.classList.add('v-class');
+    show.classList.remove('v-class');
+});
 socket.on('user-joined', id=>{
-    append(`${id} joined the chat`, 'center');
+    append(`${id} joined the chat`, 'left');
 });
 socket.on('receive', data=>{
     append(`${data.id} : ${data.message}`, `left`);
 });
 socket.on('left', id=>{
-    append(`${id} left the chat`,`center`);
+    append(`${id} left the chat`,`left`);
 });
